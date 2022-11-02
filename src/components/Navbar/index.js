68b74e3d9232371dec27af32699e4cb3c3976380
Dimas Sekti Adji <dimassekti.adji@gmail.com>
2022-11-02 16:33:40 +0700

    2 nov
    2 nov

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import NavLink from "../NavLink";
 
 export default function Header() {
+  const [token, setToken] = useState(true);
   return (
     <ul>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About</NavLink>
+      <NavLink to="/categories">Categories</NavLink>
+      {token ? <NavLink to="/about">Logout</NavLink> : <>login</>}
     </ul>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/Navbar";
 
 export default function HomePage() {
+  const [count, setCount] = useState(0);
+  console.log("count >> state");
+  console.log(count);
+  // const [name, setName] = useState();
+  // const [data, setData] = useState([
+  //   {
+  //     id: 1,
+  //     name: "Diam",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "Diam kedua",
+  //   },
+  // ]);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log("count >> useEffect", count);
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
     <div>
       <Header />
-      HomePage
+      {console.log("count >> render", count)}
+      {/* <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <p>Nama Saya : {name}</p>
+      <p>total = {count}</p>
+      <button onClick={() => setCount(count + 1)}>Click</button>
+      {data.map((data) => (
+        <ul key={data.id}>
+          <li>{data.name}</li>
+        </ul>
+      ))} */}
+
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </div>
   );
 }
